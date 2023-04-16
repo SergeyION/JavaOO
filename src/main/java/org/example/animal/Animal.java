00ba475b730e83d0cba3e1eba9fa7eb36401548e
Sergey Ionov <captor_72@bk.ru>
2023-04-16 10:58:52 +0700

    first commit

--- src/main/java/org/example/animal/Animal.java
+++ src/main/java/org/example/animal/Animal.java
@@ -5,20 +5,24 @@ package org.example;
  */
 public class Animal {
 
-    public Animal(){
-
+    public Animal() {
     }
-    public Animal(String name){
+
+    public Animal(String name) {
         this.name = name;
     }
+
     protected String name;
-    public void animalInfo(){
+
+    public void animalInfo() {
         System.out.print("Имя животного: " + name);
     }
-    public void voice(){
+
+    public void voice() {
         System.out.print("Звук!");
     }
-    public void jump(){
+
+    public void jump() {
         System.out.println("Прыжок!");
     }
 }
